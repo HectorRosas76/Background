52feefa92a6e6fc2cca735baf3b3129decc741dd
HectorRosas76 <hecrosas@uniboyaca.edu.co>
2026-04-30 15:38:54 -0500

    Operaciones Matematicas

--- microservicios/contactos_ms/public/index.php
+++ microservicios/contactos_ms/public/index.php
@@ -14,28 +14,62 @@ $app->get('/', function (Request $request, Response $response, $args) {
     return $response;
 });
 
-$app->get('/suma', function(Request $request, Response $response){
-    $parm = $request-> getQueryParams();
+$app->get('/suma', function (Request $request, Response $response) {
+    $parm = $request->getQueryParams();
     $num1 = $parm['num1'];
-     $num2 = $parm['num2'];
-$suma = $num1 + $num2;
+    $num2 = $parm['num2'];
+    $suma = $num1 + $num2;
     $response->getBody()->write("la suma de $num1 + $num2 es igual a $suma");
     return $response;
 });
 
-$app->post('/suma', function(Request $request, Response $response){
-   $body = $request ->getBody()->getContents();
-   $data = json_decode($body, true); 
-   $num1 = $data['num1'];
-     $num2 = $data['num2'];
-$suma = $num1 + $num2;
-$result = [
-'num1' => $num1,
-'num2' => $num2,
-'resultado' => $suma
-];
+$app->post('/suma', function (Request $request, Response $response) {
+    $body = $request->getBody()->getContents();
+    $data = json_decode($body, true);
+    $num1 = $data['num1'];
+    $num2 = $data['num2'];
+    $suma = $num1 + $num2;
+    $result = [
+        'num1' => $num1,
+        'num2' => $num2,
+        'resultado' => $suma
+    ];
     $response->getBody()->write(json_encode($result));
-    return $response -> withHeader('Content-Type', 'application/json');
+    return $response->withHeader('Content-Type', 'application/json');
 });
+
+$app->post('/multiplicar', function (Request $request, Response $response) {
+    $body = $request->getBody()->getContents();
+    $data = json_decode($body, true);
+    $num1 = $data['num1'];
+    $num2 = $data['num2'];
+    $result = [
+        'num1' => $num1,
+        'num2' => $num2,
+        'resultado' => $num1 * $num2
+    ];
+    $response->getBody()->write(json_encode($result));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+
+$app->post('/dividir', function (Request $request, Response $response) {
+    $body = $request->getBody()->getContents();
+    $data = json_decode($body, true);
+    $num1 = $data['num1'];
+    $num2 = $data['num2'];
+    if ($num2 == 0){
+        $response->getBody()->write("NO se puede dividir por 0");
+    return $response->withStatus(400);
+    }
+    $result = [
+        'num1' => $num1,
+        'num2' => $num2,
+        'resultado' => $num1 / $num2
+    ];
+    $response->getBody()->write(json_encode($result));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
 
 $app->run();
